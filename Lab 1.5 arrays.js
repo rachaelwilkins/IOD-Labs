afb43a9d4594892f2c11b234b310223562dec860
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-03-07 17:42:55 +1300

    ready for marking

--- Lab 1.5 arrays.js
+++ Lab 1.5 arrays.js
@@ -1,7 +1,9 @@
+// try creating an array that has 5 elements. Replace the value of the element at position 1 and 4. Add a new element to the beginning of the array. Remove the element at the end of the array. Print the array to the console.
+
 const schedule = ["Read", "Eat", "Work", "Train", "Study", "Nap"];
 
 schedule[1] = "Have Coffee"
-schedule[4] = "Read a Book"
+schedule[4] = "Watch Netflix"
 
 schedule.unshift("Wake Up")
 
