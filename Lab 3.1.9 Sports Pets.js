1a62ddeeba01a85ebb37423ae6c8a004b34bb10a
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-03-20 16:00:32 +1300

    total WIP do not look

---
+++ Lab 3.1.9 Sports Pets.js
@@ -0,0 +1,17 @@
+// a) Create a new moreSports variable equal to teamSports and add some new sport values to it (using push and unshift)
+// b) Create a new dog2 variable equal to dog1 and give it a new value
+// c) Create a new cat2 variable equal to cat1 and change its name property
+// d) Print the original teamSports, dog1 and cat1 variables to the console. Have they changed? Why?
+// e) Change the way the moreSports and cat2 variables are created to ensure the originals remain independent
+
+
+let teamSports = ['Hockey', 'Cricket', 'Volleyball'];
+
+moreSports.unshift('Rugby', 'Tennis', 'Badminton');
+moreSports.push('Golf');
+
+let dog1 = 'Bingo';
+let cat1 = { name: 'Fluffy', breed: 'Siberian' };
+
+let dog2 = 'Travis';
+let cat2 = { name: 'Charlie', breed: 'Siberian'};
