7c47a224f0f07c932b46554a888c89ac7cc3f776
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-03-28 20:02:07 +1300

    WIP

---
+++ Module3/Lab 3.1.06 Princess Bride.js
@@ -0,0 +1,30 @@
+// 3.1.06 - Princess Bride
+
+// a) Complete the inigo object by adding a lastName property and including it in the greeting. 
+// b) Complete getCatchPhrase so that if the person argument has 6 fingers, it instead prints his famous catch phrase to the console.
+// c) Update getCatchPhrase to use arrow function syntax and a conditional operator.
+
+    const westley = {
+    name: 'Westley',
+    numFingers: 5
+}
+    const rugen = {
+    name: 'Count Rugen',
+    numFingers: 6
+}
+    const inigo = {
+    firstName: 'Inigo',
+    lastName: 'Montoya',
+    greeting(person) { 
+        let greeting = `Hello ${person.name}, my name is ${this.firstName}. `;
+        console.log(greeting + this.getCatchPhrase(person));
+    },
+    getCatchPhrase(person) {
+        return 'Nice to meet you.';
+        }
+}
+
+    inigo.greeting(westley)
+    inigo.greeting(rugen)
+
+  
