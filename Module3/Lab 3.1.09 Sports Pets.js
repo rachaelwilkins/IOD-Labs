31edcc066a231b397bbfd9df61926beba95ff86f
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-04-02 18:01:11 +1300

    ready for marking

--- Module3/Lab 3.1.09 Sports Pets.js
+++ Module3/Lab 3.1.09 Sports Pets.js
@@ -3,13 +3,11 @@
 // a) Create a new moreSports variable equal to teamSports and add some new sport values to it (using push and unshift)
 // b) Create a new dog2 variable equal to dog1 and give it a new value
 // c) Create a new cat2 variable equal to cat1 and change its name property
-// d) Print the original teamSports, dog1 and cat1 variables to the console. Have they changed? Why?
-// e) Change the way the moreSports and cat2 variables are created to ensure the originals remain independent
-
+// d) Print the original teamSports, dog1 and cat1 variables to the console. Have they changed? Why? Yep, because they are printing as the second variables.
 
 let teamSports = ['Hockey', 'Cricket', 'Volleyball'];
 
-let moreSports = teamSports;
+//let moreSports = teamSports;
 moreSports.unshift('League');
 moreSports.push('Golf');
 
@@ -24,3 +22,11 @@ cat2.name = 'Charby';
 console.log(teamSports);
 console.log(dog1);
 console.log(cat1);
+
+// e) Change the way the moreSports and cat2 variables are created to ensure the originals remain independent
+
+let moreSports = ['Golf', 'League'];
+let cat2 = { name: 'Charby', breed: 'Siberian' };
+
+console.log(moreSports);
+console.log(cat2);
