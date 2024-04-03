2641d4dbee0b3f6df99a5004e8a690212d5fb5d0
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-04-03 16:41:11 +1300

    WIP

---
+++ Module3Intermediate/Lab 3.2.03 Animals.js
@@ -0,0 +1,20 @@
+//a) Add 2 new values to the end
+//b) Add 2 new values to the beginning
+//c) Sort the values alphabetically
+//d) Write a function replaceMiddleAnimal(newValue) that replaces the value in the middle of the animals array with newValue
+//e) Write a function findMatchingAnimals(beginsWith) that returns a new array containing all the animals that begin 
+//with the beginsWith string. Try to make it work regardless of upper/lower case.
+
+const animals = ['Tiger', 'Giraffe']
+
+animals.push('Red Panda', 'Otter')
+
+animals.unshift('Bear', 'Fox')
+
+animals.sort();
+
+
+
+
+
+console.log(animals)
