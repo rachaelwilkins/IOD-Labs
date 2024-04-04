160b829a45e67257947ba23d5015061f0419bd5e
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-04-04 18:30:25 +1300

    ready for marking

--- Module3Fundamentals/Lab 3.1.03.js
+++ Module3Fundamentals/Lab 3.1.03.js
@@ -1,8 +1,8 @@
 // 3.3 Which of the following console.log messages will print? Why?
 
 if (0) console.log('#1 zero is true')
-if ("0") console.log('#2 zero is true') //prints
+if ("0") console.log('#2 zero is true') //prints. "0" is a string, since it's not empty, it is true. 
 if (null) console.log('null is true')
-if (-1) console.log('negative is true') //prints
-if (1) console.log('positive is true') //prints
+if (-1) console.log('negative is true') //prints. Any Non-zero numbers evaluate to true.
+if (1) console.log('positive is true') //prints. Same as above. 
 
