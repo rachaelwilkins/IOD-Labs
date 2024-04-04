36454e2d113ebdd11987410b04b49ba9f3881dec
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-04-04 15:56:58 +1300

    ready for marking

--- Module3Fundamentals/Lab 3.1.05.js
+++ Module3Fundamentals/Lab 3.1.05.js
@@ -1,5 +1,21 @@
 // 3.5 Rewrite the following function using: a) function expression syntax, and b) arrow function syntax. Test each version to make sure they work the same.
 
-function getGreeting(name) {
-    return 'Hello ' + name + '!';
-    }
+//function getGreeting(name) {
+//    return 'Hello ' + name + '!';
+//    }
+
+// function expression syntax
+const getGreeting = function() {
+    console.log("Hello, Layne!");
+  };
+  
+  getGreeting(); // Hello, Layne! 
+
+
+
+// arrow function syntax
+const getGreeting = name => {
+    console.log(`Hello, ${name}!`);
+      };
+      
+    getGreeting('Layne'); // Hello, Layne!
