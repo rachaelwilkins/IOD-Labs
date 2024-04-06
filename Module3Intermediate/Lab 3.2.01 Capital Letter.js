c8712f72cd2d729a7168917d2a045e7161b01fee
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-04-06 16:02:03 +1300

    yet to start

---
+++ Module3Intermediate/Lab 3.2.01 Capital Letter.js
@@ -0,0 +1,8 @@
+/* Create a function that takes a string as a parameter and returns the string with the first character of each word changed into a capital letter, as in the example below. 
+Test it with different strings */
+
+
+
+
+
+console.log(ucFirstLetters("los angeles") ) //Los Angeles
