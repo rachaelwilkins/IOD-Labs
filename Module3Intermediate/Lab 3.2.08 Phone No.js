816961704452e83c63edc0a38acf95551631b7dc
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-04-06 16:33:37 +1300

    WIP

--- Module3Intermediate/Lab 3.2.08 Phone No.js
+++ Module3Intermediate/Lab 3.2.08 Phone No.js
@@ -1,11 +1,11 @@
-const phoneBookABC = new Map() //an empty map to begin with
+const phoneBookABC = new Map() 
 phoneBookABC.set('Annabelle', '0412312343') 
 phoneBookABC.set('Barry', '0433221117')
 phoneBookABC.set('Caroline', '0455221182')
 
 //a) Create a new phoneBookDEF Map to store names beginning with D, E or F//
 
-const phoneBookDEF = new Map() //an empty map to begin with
+const phoneBookDEF = new Map() 
 phoneBookDEF.set('Donna', '0412345896') 
 phoneBookDEF.set('Emma', '0433231412')
 phoneBookDEF.set('Francis', '0455275488')
@@ -18,11 +18,14 @@ console.log(phoneBookDEF);
 //c) Update the phone number for Caroline//
 
 
+
 //d) Write a function printPhoneBook(contacts) that prints the names and phone numbers in the given Map//
 
 
 //e) Combine the contents of the two individual Maps into a single phoneBook Map//
+//f) Print out the full list of names in the combined phone book.// 
 
+let phoneBookABCDEF = new Map([...phoneBookABC, ...phoneBookDEF]);
 
+console.log(phoneBookABCDEF);
 
-//f) Print out the full list of names in the combined phone book//
