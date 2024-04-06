33d5c40e4ef3002e8f36329d4070374df10878c1
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-04-06 16:44:02 +1300

    WIP

--- Module3Intermediate/Lab 3.2.10 Date.js
+++ Module3Intermediate/Lab 3.2.10 Date.js
@@ -12,19 +12,14 @@ console.log(today.getSeconds() + ' seconds have passed so far today')
 
 // c) Calculate and print your age as: 'I am x years, y months and z days old'//
 
+
 // d) Write a function daysInBetween(date1, date2) which calculates and returns the amount of days in between the two given dates //
 
-function daysInBetween(date1, date2) {
-    var date1 = new Date('12/25/2024');
-    var date2 = new Date('04/06/2024');
-    var daysInBetween = (date1-date2) / (1000*60*60*24);
-}
-
-console.log( daysInBetween(date1, date2) );
-
-
-/* const date1 = new Date('12/25/2024');
+const date1 = new Date('12/25/2024');
 const date2 = new Date('04/06/2024');
 var totalDays = (date1-date2) / (1000*60*60*24);
-console.log(totalDays); // 263 days // */
+console.log(totalDays); // 263 days 
+
+
+
 
