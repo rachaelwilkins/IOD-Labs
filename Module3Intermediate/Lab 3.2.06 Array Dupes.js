f86760affc689455f3d31771265ef3656eb3b30a
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-04-06 16:02:33 +1300

    yet to start

---
+++ Module3Intermediate/Lab 3.2.06 Array Dupes.js
@@ -0,0 +1,12 @@
+/*Create a function unique(duplicatesArray) which takes an array parameter that may include duplicates. Your function should return an array containing only the unique values 
+from duplicatesArray. 
+Test with the following arrays and create another one of your own. */
+
+const colours = ['red', 'green', 'blue', 'yellow', 'orange', 'red', 'blue', 'yellow']
+const testScores = [55, 84, 97, 63, 55, 32, 84, 91, 55, 43]
+
+
+
+console.log(unique(colours)) 
+console.log(unique(testScores)) 
+
