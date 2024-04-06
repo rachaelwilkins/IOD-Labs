4cbe1e33251e3c51837935a5160da4c41c825eee
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-04-06 16:01:52 +1300

    yet to start

---
+++ Module3Advanced/Lab 3.3.11 Pizza.js
@@ -0,0 +1,15 @@
+/* Mimic a pizza making procedure, by writing code that prints statements in the below order:
+- Started preparing pizza
+- Made the base
+- Added the sauce and cheese
+- Added the pizza toppings
+- Cooked the pizza
+- Pizza is ready
+
+Task 1: Create 6 JS functions which print the pizza processing statements and call those functions in sequence. Use a mix of function declarations, expressions and arrow functions.
+Task 2: Make the functions asynchronous by using setTimeout with different time durations, maintaining the right order.
+Task 3: Modify the asynchronous functions to use Promises and achieve the required result.
+Task 4: Modify the functions to use async/await and achieve the required result */
+
+
+
