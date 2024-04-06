1965d36d9cd6e8ac4d094d9b9b319625e9ac0f3e
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-04-06 16:11:44 +1300

    WIP

--- Module3Intermediate/Lab 3.2.08 Phone No.js
+++ Module3Intermediate/Lab 3.2.08 Phone No.js
@@ -1,14 +1,28 @@
-/* 
-a) Create a new phoneBookDEF Map to store names beginning with D, E or F
-b) Initialise the contents of phoneBookDEF by passing in an array of keys/values
-c) Update the phone number for Caroline
-d) Write a function printPhoneBook(contacts) that prints the names and phone 
-numbers in the given Map
-e) Combine the contents of the two individual Maps into a single phoneBook Map
-f) Print out the full list of names in the combined phone book */ 
-
 const phoneBookABC = new Map() //an empty map to begin with
 phoneBookABC.set('Annabelle', '0412312343') 
 phoneBookABC.set('Barry', '0433221117')
 phoneBookABC.set('Caroline', '0455221182')
 
+//a) Create a new phoneBookDEF Map to store names beginning with D, E or F//
+
+const phoneBookDEF = new Map() //an empty map to begin with
+phoneBookDEF.set('Donna', '0412345896') 
+phoneBookDEF.set('Emma', '0433231412')
+phoneBookDEF.set('Francis', '0455275488')
+
+console.log(phoneBookDEF);
+
+//b) Initialise the contents of phoneBookDEF by passing in an array of keys/values//
+
+
+//c) Update the phone number for Caroline//
+
+
+//d) Write a function printPhoneBook(contacts) that prints the names and phone numbers in the given Map//
+
+
+//e) Combine the contents of the two individual Maps into a single phoneBook Map//
+
+
+
+//f) Print out the full list of names in the combined phone book//
