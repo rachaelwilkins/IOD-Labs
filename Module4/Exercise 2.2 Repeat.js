c140c8e0739c5b722430b52737d055ec24b6980f
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-04-10 18:12:32 +1200

    not confident

---
+++ Module4/Exercise 2.2 Repeat.js
@@ -0,0 +1,14 @@
+
+const data = [{ name: "bob", age: 23 },
+  { name: "alice", age: 39 },];
+
+
+  
+
+  function addCard() {
+        const template = document.getElementById("card-template").content.cloneNode(true);
+        template.querySelector(".card-title").innerText = "Bob", "Alice";
+        template.querySelector(".card-text").innerText = "23", "39";
+        document.querySelector("#card-list").appendChild(template);
+      }
+      addCard();
