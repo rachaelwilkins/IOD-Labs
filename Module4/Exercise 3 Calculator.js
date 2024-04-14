91f0198c88a58cfb70bde324ccea00256b7cd7c2
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-04-14 14:09:39 +1200

    WIP

---
+++ Module4/Exercise 3 Calculator.js
@@ -0,0 +1,17 @@
+
+function myFunctionAdd(p1, p2) {
+    return p1 + p2
+  }
+  
+  function myFunctionTimes(p1, p2) {
+    return p1 * p2
+  }
+
+      
+  function myFunctionSub(p1, p2) {
+        return p1 - p2
+  }   
+      
+  function myFunctionDiv(p1, p2) {
+      return p1 / p2
+  } 
