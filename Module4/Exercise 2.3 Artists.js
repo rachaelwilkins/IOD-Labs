0cc820df8c311eb9352690c0d9641566ccc7c607
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-04-14 15:31:17 +1200

    delete

--- Module4/Exercise 2.3 Artists.js
+++
@@ -1,9 +0,0 @@
-const artist = {
-    name: "Van Gogh",
-    portfolio:[
-    {title: "portrait", url:"https://collectionapi.metmuseum.org/api/collection/v1/iiif/436532/1671316/main-image"},
-    {title: "sky", url:"https://mymodernmet.com/wp/wp-content/uploads/2020/11/White-house-night-van-goh-worldwide-2.jpg" },
-    ]
-   }
-   
-   console.log(artist);
