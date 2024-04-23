ff9dafbf27f7dc1b04951da795fdf93a7bf07755
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-04-23 18:02:07 +1200

    WIP help haha

--- Mini-Project-1/Mini-Project-1-quiz.js
+++ Mini-Project-1/Mini-Project-1-quiz.js
@@ -1,7 +1,19 @@
-const catsPromise = fetch('https://api.thecatapi.com/v1/breeds')
+fetch('https://api.thecatapi.com/v1/breeds')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
 
-class Cats {
-    constructor() {
-        this.name
+class Cat {
+  constructor(name, child_friendly, dog_friendly, indoor, energy_level, vocalisation, affection_level, grooming) {
+    this.name = name;
+    this.child_friendly = child_friendly;
+    this.dog_friendly = dog_friendly;
+    this.indoor = indoor;
+    this.energy_level = energy_level;
+    this.vocalisation = vocalisation;
+    this.affection_level = affection_level;
+    this.agrooming = grooming;
     }
-}
+  }
+
+  
