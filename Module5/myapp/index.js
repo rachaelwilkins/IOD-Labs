e051f935f330f00e2e6cef79ab4610a5bf5d7a25
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-05-02 20:45:45 +1200

    commit

--- Module5/myapp/index.js
+++ Module5/myapp/index.js
@@ -1,6 +1,9 @@
 const express = require("express"); 
-
+const cors = require("cors");
 const app = express(); 
+app.use(cors())
+const ex2 = require("./Routes/Exercise 5.2")
+app.use(`/api/ex2`, ex2)
 
 const port = 3000; 
 
