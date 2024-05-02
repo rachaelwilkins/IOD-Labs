8468221f1d57ed75a455f365166e4b77067372b8
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-05-02 18:43:08 +1200

    wip

--- Module5/myapp/Lab Exercises/Exercise 5.6/calculatorController.js
+++ Module5/myapp/Lab Exercises/Exercise 5.6/calculatorController.js
@@ -16,4 +16,21 @@ function Multiply(req, res)
     res.send({result: sum})
 }
 
-module.exports = {Add, Multiply}
+function Subtract(req, res)
+{
+    let num1 = parseInt(req.query.num1);
+    let num2 = parseInt(req.query.num2);
+    let sum = num1 - num2
+    res.status = 200
+    res.send({result: sum})
+}
+
+function Divide(req, res)
+{
+    let num1 = parseInt(req.query.num1);
+    let num2 = parseInt(req.query.num2);
+    let sum = num1 / num2
+    res.status = 200
+    res.send({result: sum})
+}
+module.exports = {Add, Multiply, Subtract, Divide}
