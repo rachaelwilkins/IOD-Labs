13ea48577690cfc4096175bbbf07201c68f70cd5
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-05-04 16:13:50 +1200

    commit

--- Module5/myapp/index.js
+++ Module5/myapp/index.js
@@ -1,9 +1,21 @@
+//NOT WORKING UNSURE WHY PLEASE HELP
+
 const express = require("express"); 
 const cors = require("cors");
 const app = express(); 
+
+const swaggerUi = require('swagger-ui-express');
+swaggerDocument = require('./swagger.json');
+
 app.use(cors())
 const ex2 = require("./Routes/Exercise 5.2")
 app.use(`/api/ex2`, ex2)
+
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+ );
 
 const port = 3000; 
 
@@ -14,3 +26,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
