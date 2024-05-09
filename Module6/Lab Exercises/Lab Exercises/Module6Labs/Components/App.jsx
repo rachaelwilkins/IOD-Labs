32de65741226b11adb8ca608b7851056aaf3fd95
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-05-09 17:28:15 +1200

    commit

--- Module6/Lab Exercises/Lab Exercises/Module6Labs/Components/App.jsx
+++ Module6/Lab Exercises/Lab Exercises/Module6Labs/Components/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import '../Components/Exercise 6.1 Component'
+import './Exercise 6.1 Component'
+import Lab1Greeting from './Exercise 6.1 Component'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+          <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
@@ -29,9 +30,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <Greeting name='John'>
+      <Lab1Greeting name='John'>
         <p>This is React - hope you like it as much as Gareth does.</p>
-      </Greeting>
+      </Lab1Greeting>
     </>
   )
 }
