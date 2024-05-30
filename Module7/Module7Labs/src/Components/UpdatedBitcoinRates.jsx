865368d27a69205244c619c55dc9c70276102c9f
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-05-30 20:40:40 +1200

    WIP

---
+++ Module7/Module7Labs/src/Components/UpdatedBitcoinRates.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+
+const currencies = ['USD', 'AUD', 'NZD', 'GBP', 'EUR', 'SGD'];
+
+function BitcoinRates() {
+
+    const [currency, setCurrency] = useState(currencies[0]);
+
+    const [rate, setRate] = useState('');
+
+    useEffect(() => {
+        if (url) {
+        let ignore = false;
+        fetch(url)
+        .then(response => response.json())
+        .then(json => {
+        if (!ignore) {
+        setData(json);
+        }
+        });
+       
+
+        return () => {
+            ignore = true;
+            };
+            }
+            }, [url]); // re-run effect if url changes
+    
+    const options = currencies.map(curr => <option value={curr} key={curr}>{curr}</option>);
+    return (
+        <div className="BitcoinRates componentBox">
+            <h1>Exercise 6.1 - BitCoin Rates</h1>
+            <label>Set currency:
+                <select value={currency} onChange={e => setCurrency(e.target.value)}>
+                    {options}
+                </select>
+            </label>
+            <p>Rate: {rate}</p>
+        </div>
+    )
+}
+
+export default BitcoinRates
